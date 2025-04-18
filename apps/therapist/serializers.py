2595e915a13770d4d1fa7916cddba90298e3cf72
sohila <sohilamustafa68@gmail.com>
2025-04-18 22:45:01 +0200

    added permissions to therapist mode

--- apps/therapist/serializers.py
+++ apps/therapist/serializers.py
@@ -5,15 +5,50 @@ class TherapistReviewSerializer(serializers.ModelSerializer):
     therapist_name = serializers.CharField(source='therapist.name',read_only=True)
     class Meta:
         model = TherapistReview
-        fields = ['id', 'therapist_name', 'patient', 'review', 'rating']
+        fields = ['therapist_name', 'patient', 'review', 'rating']
 class TherapistContactSerializer(serializers.ModelSerializer):
     therapist_name = serializers.CharField(source='therapist.name',read_only=True)
     class Meta:
         model = TherapistContact
-        fields = ['id','therapist_name','country_code','contact']
-class TherapistSerializer(serializers.ModelSerializer):
+        fields = ['therapist_name','country_code','contact']
+
+class TherapistAdminSerializer(serializers.ModelSerializer):
     reviews = TherapistReviewSerializer(source='therapist_reviews',read_only=True,many=True)
     contact = TherapistContactSerializer(source='therapist_contact',read_only=True,many=True)
     class Meta:
         model = Therapist
         fields = '__all__'
+
+
+class TherapistSerializer(serializers.ModelSerializer):
+    reviews = TherapistReviewSerializer(source='therapist_reviews',read_only=True,many=True)
+    contact = TherapistContactSerializer(source='therapist_contact',read_only=True,many=True)
+    class Meta:
+        model = Therapist
+        fields = ['name','email','specialty','photo','working_hours','joining_date','certificates','experience','is_active','reviews','contact']
+
+    def validate_email(self, value):
+        if Therapist.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists")
+        return value
+    
+    def validate_working_hours(self, value):
+        if value.hour < 0 or value.hour > 23 :
+            raise serializers.ValidationError("Invalid working hours")
+        return value
+    
+
+    def validate(self, attrs):
+        speciality = attrs.get('specialty')
+        if speciality and len(speciality) < 5:
+            raise serializers.ValidationError("Speciality must be at least 5 characters")
+        
+        name = attrs.get('name')
+        if not name or name.strip() == '':
+            raise serializers.ValidationError("Name must be at least 5 characters")
+        return attrs
+
+    
+    def create(self, validated_data):
+        therapist = Therapist.objects.create(**validated_data)
+        return therapist
--- apps/therapist/views.py
+++ apps/therapist/views.py
@@ -1,16 +1,48 @@
 from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from .models import *
 from .serializers import *
+from .permissions import IsTherapist
 
 class TherapistViewset(viewsets.ModelViewSet):
     queryset = Therapist.objects.all()
-    serializer_class = TherapistSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        elif self.action in ['update','partial_update','destroy']:
+            return [IsTherapist()]
+        else:
+            return [IsAuthenticated()]
+        
+    def get_serializer_class(self):
+        if self.request.user.is_staff or self.request.user.is_superuser:
+            return TherapistAdminSerializer
+        return TherapistSerializer
 
 class TherapistContactViewset(viewsets.ModelViewSet):
     queryset = TherapistContact.objects.all()
     serializer_class = TherapistContactSerializer
 
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        elif self.action in ['update','partial_update','destroy']:
+            return [IsTherapist()]
+        else:
+            return [IsAuthenticated()]
+
 class TherapistReviewViewset(viewsets.ModelViewSet):
     queryset = TherapistReview.objects.all()
     serialzer_class = TherapistReviewSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        elif self.action in ['update','partial_update','destroy']:
+            return [IsTherapist()]
+        else:
+            return [IsAuthenticated()]
